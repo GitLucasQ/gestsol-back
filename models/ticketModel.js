6fdd5781a9685ef079701459178588b246f15f60
LucasQ <lucass2408@hotmail.com>
2021-07-15 19:31:02 -0500

    Pre aprobacion del sistema

--- models/ticketModel.js
+++ models/ticketModel.js
@@ -32,7 +32,7 @@ module.exports = {
 
     ticketsPorUsuario(id_usuario) {
         return new Promise((resolve, reject) => {
-            connection.query("select trim(NOMBRE_USUARIO_ASIGNADO) persona, count(NRO_TICKET) cantidad \
+            connection.query("select trim(NOMBRE_USUARIO_ASIGNADO) persona, count(*) cantidad \
             from gestsol.seguimiento a \
             inner join gestsol.usuario b \
             on a.id_usuario_asignado = b.id_usuario \
@@ -53,7 +53,7 @@ module.exports = {
             inner join gestsol.usuario b \
             on a.id_usuario_asignado = b.id_usuario \
             where b.area = (select distinct area from usuario where tipo_usuario = ?) \
-            group by NOMBRE_USUARIO_ASIGNADO;",
+            group by NOMBRE_USUARIO_ABORDAJE;",
                 [id_usuario],
                 (err, result) => {
                     if (err) reject(err), console.log(err)
